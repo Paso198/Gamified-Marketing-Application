a86147678821fe618dfb84ff23d511e07c729e16
Antonio_Dee <antoniodesantis05@outlook.it>
2020-12-14 00:09:43 +0100

    started implementing QuestionnaireService

--- ServerSide/Questionairre/src/main/java/it/polimi/db2/questionnaire/repositories/QuestionnaireRepository.java
+++ ServerSide/Questionairre/src/main/java/it/polimi/db2/questionnaire/repositories/QuestionnaireRepository.java
@@ -1,5 +1,8 @@
 package it.polimi.db2.questionnaire.repositories;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import it.polimi.db2.questionnaire.model.Questionnaire;
 
 @Repository
 public interface QuestionnaireRepository extends JpaRepository<Questionnaire, Long> {
+
+	Optional <Questionnaire> findByDate(LocalDate date); //TODO Query?
 
 }
--- ServerSide/Questionairre/src/main/java/it/polimi/db2/questionnaire/services/QuestionnaireService.java
+++ ServerSide/Questionairre/src/main/java/it/polimi/db2/questionnaire/services/QuestionnaireService.java
@@ -1,7 +1,14 @@
 package it.polimi.db2.questionnaire.services;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import it.polimi.db2.questionnaire.dto.requests.AddQuestionnaireRequest;
+import it.polimi.db2.questionnaire.dto.responses.QuestionnaireResponse;
+import it.polimi.db2.questionnaire.mappers.QuestionnaireMapper;
+import it.polimi.db2.questionnaire.model.Questionnaire;
 import it.polimi.db2.questionnaire.repositories.QuestionnaireRepository;
 import lombok.AllArgsConstructor;
 
@@ -10,8 +17,30 @@ import lombok.AllArgsConstructor;
 public class QuestionnaireService {
 	
 	private final QuestionnaireRepository questionnaireRepository;
+	private final QuestionnaireMapper questionnaireMapper;
 	
-	public void addQuestionnaire() {
-		
+	public void addQuestionnaire(AddQuestionnaireRequest addQuestionnaireRequest) {
+		questionnaireRepository.save(questionnaireMapper.toQuestionnaire(addQuestionnaireRequest));
+	}
+	
+	public QuestionnaireResponse getQuestionnaireOfTheDay() { //.now() not tested
+		Questionnaire questionnaire = questionnaireRepository.findByDate(LocalDate.now()).orElseThrow(/*TODO exception*/);
+		return questionnaireMapper.toQuestionnaireResponse(questionnaire);
+	}
+	
+	public Optional<Questionnaire> getQuestionnaire(Long id) {
+		return questionnaireRepository.findById(id);
+	}
+	
+	public void deleteQuestionnaire(Long id) {
+		verifyOld(id);
+		questionnaireRepository.deleteById(id);
+	}
+	
+	public void verifyOld(Long id) {
+		Questionnaire questionnaire = getQuestionnaire(id).orElseThrow(/*TODO exception*/);
+		if(questionnaire.getDate().isBefore(LocalDate.now())) {
+			//TODO exception
+		}
 	}
 }
