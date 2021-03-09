ea0f1d854e20c77349178f312c26da779e144c63
Antonio_Dee <antoniodesantis05@outlook.it>
2021-03-09 18:49:49 +0100

    fixes

--- ServerSide/Questionairre/src/main/java/it/polimi/db2/questionnaire/mappers/QuestionMapper.java
+++ ServerSide/Questionairre/src/main/java/it/polimi/db2/questionnaire/mappers/QuestionMapper.java
@@ -2,15 +2,17 @@ package it.polimi.db2.questionnaire.mappers;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-import it.polimi.db2.questionnaire.dto.responses.AnswerResponse;
-import it.polimi.db2.questionnaire.model.Answer;
+import it.polimi.db2.questionnaire.dto.responses.QuestionResponse;
+import it.polimi.db2.questionnaire.model.Question;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public abstract class QuestionMapper {
 
-	//TODO
-	public abstract AnswerResponse toAnswerResponse(Answer answer);
+	@Mapping(target="id", source="id")
+	@Mapping(target="text", source="text")
+	public abstract QuestionResponse toQuestionResponse(Question question);
 }
